a2c487860467a624d05751c5614fb62d15e98ec5
Ratul Minhaz <minhaz.ratul@gmail.com>
2025-03-19 16:50:05 +0100

    Create swimlane component

--- karma/src/routes/+layout.svelte
+++ karma/src/routes/+layout.svelte
@@ -1,22 +1,13 @@
 <script lang="ts">
-	import Header from './Header.svelte';
-	import '../app.css';
+	import "../app.css"
 
-	let { children } = $props();
+	let { children } = $props()
 </script>
 
 <div class="app">
-	<Header />
-
 	<main>
 		{@render children()}
 	</main>
-
-	<footer>
-		<p>
-			visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to learn about SvelteKit
-		</p>
-	</footer>
 </div>
 
 <style>
