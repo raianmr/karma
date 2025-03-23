792fa665bc5ec29d51b70ac8d85e08c9012358fc
Ratul Minhaz <minhaz.ratul@gmail.com>
2025-03-23 02:16:39 +0100

    WIP: Restructuring project structure

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<!-- <script lang="ts">
+	import '../app.css';
+	let { children } = $props();
+</script>
+
+{@render children()} -->
+
+<script lang="ts">
+	import '../app.css';
+
+	let { children } = $props();
+</script>
+
+<div class="app">
+	<main>
+		{@render children()}
+	</main>
+</div>
+
+<style>
+</style>
